4b8d6eb56d288be3c8eba301940295d70e27092f
MatthewPangihutan <143679163+MatthewPangihutan@users.noreply.github.com>
2023-10-01 14:36:58 +0700

    12S23031_Matthew Pangihutan

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,37 @@
-// NIM - Your Name
+// 12S23031-Matthew pangihutan
+import java.util.*;
+import java.lang.Math;
+
+public class P02_01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namatugas, nim, dosenpengampu, deadline, nimnamapengumpul, status, hasil1;
+        int tingkatkesulitan, harihinggadeadline;
+        double prioritas;
+        
+        namatugas = input.nextLine();
+        nim = input.nextLine();
+        dosenpengampu = input.nextLine();
+        deadline = input.nextLine();
+        nimnamapengumpul = input.nextLine();
+        status = input.nextLine();
+        tingkatkesulitan = input.nextInt();
+        harihinggadeadline = input.nextInt();
+        prioritas = tingkatkesulitan * (1.0 / harihinggadeadline);
+        System.out.println("Prioritas :" + prioritas);
+        if (prioritas > 3) {
+            hasil1 = "Penting! Anda harus mengerjakan tugas ini segera.";
+        } else {
+            if (prioritas >= 1.5 && prioritas <= 3) {
+                hasil1 = "Tugas ini memiliki prioritas menengah.";
+            } else {
+                if (prioritas < 1.5) {
+                    hasil1 = "Tugas ini relatif ringan, namun jangan tunda terlalu lama.";
+                }
+            }
+        }
+        System.out.println(namatugas + "|" + nim + "|" + dosenpengampu + "|" + deadline + "|" + nimnamapengumpul + "|" + status + "|" + namatugas + "|" + tingkatkesulitan + "|" + harihinggadeadline + "|" + status);
+    }
+}
+
--- P02_02.java
+++ P02_02.java
@@ -1,1 +1,37 @@
-//NIM -Nama
+// 12S23031-Matthew Pangihutan
+import java.util.*;
+import java.lang.Math;
+
+public class P02_02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namaGudang, kapasitasGudang, jumlahBukuSaatIni, aC, lantai, ketersediaanTeknologi;
+        
+        System.out.println("NamaGudang");
+        namaGudang = input.nextLine();
+        System.out.println("KapasitasGudang");
+        kapasitasGudang = input.nextLine();
+        System.out.println("JumlahBukuSaatIni");
+        jumlahBukuSaatIni = input.nextLine();
+        System.out.println("AC");
+        aC = input.nextLine();
+        System.out.println("Lantai");
+        lantai = input.nextLine();
+        System.out.println("KetersediaanTeknologi");
+        ketersediaanTeknologi = input.nextLine();
+        if (SkorGudang >= 40) {
+            Hasil = "Gudang Elite";
+        } else {
+            if (SkorGudang >= 25) {
+                Hasil = "Gudang Standar";
+            } else {
+                if (SkorGudang >= 25) {
+                    Hasil = "Gudang Perlu Peringkatan";
+                }
+            }
+        }
+        System.out.println(namaGudang + "|" + kapasitasGudang + "|" + jumlahBukuSaatIni + "|" + aC + "|" + lantai + "|" + ketersediaanTeknologi);
+    }
+}
+
